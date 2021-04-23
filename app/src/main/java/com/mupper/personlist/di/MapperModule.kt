50588d170f9d3e08e1ad99d63235347d87f1994b
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2021-04-22 18:18:08 -0700

    DomainPersonToUIPersonMapper implemented

--- app/src/main/java/com/mupper/personlist/di/MapperModule.kt
+++ app/src/main/java/com/mupper/personlist/di/MapperModule.kt
@@ -1,12 +1,14 @@
 package com.mupper.personlist.di
 
-import com.mupper.personlist.presentation.mapper.DomainPersonToUIPersonMapper
 import com.mupper.personlist.presentation.mapper.DomainPersonToUIPersonMapperImpl
 import com.mupper.personlist.utils.ListMapperImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Module
 @InstallIn(ViewModelComponent::class)
@@ -15,4 +17,9 @@ object MapperModule {
   fun providesDomainPersonToUIPersonListMapper(
     domainPersonToUIPersonMapperImpl: DomainPersonToUIPersonMapperImpl
   ) = ListMapperImpl(domainPersonToUIPersonMapperImpl)
+
+  @Provides
+  fun providesBirthdayDateFormat(
+    appLocale: Locale
+  ): DateFormat = SimpleDateFormat("M/d/yy", appLocale)
 }
---
+++ app/src/test/java/com/mupper/personlist/presentation/mapper/DomainPersonToUIPersonMapperImplTest.kt
@@ -0,0 +1,84 @@
+package com.mupper.personlist.presentation.mapper
+
+import com.mupper.personlist.domain.entity.DomainPerson
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.RelaxedMockK
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.Before
+import org.junit.Test
+import java.text.DateFormat
+import java.util.*
+import kotlin.random.Random
+
+class DomainPersonToUIPersonMapperImplTest {
+    @RelaxedMockK
+    lateinit var birthdayDateFormat: DateFormat
+
+    @RelaxedMockK
+    lateinit var domainPerson: DomainPerson
+
+    @InjectMockKs
+    lateinit var domainPersonToUIPersonMapperImpl: DomainPersonToUIPersonMapperImpl
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun mapShouldReturnUIPersonWithExpectedIdGivenIdOnDomainPersonReturnsExpectedId() {
+        // Given
+        val expectedId = "${Random.nextInt()}"
+        every { domainPerson.id } returns expectedId
+
+        // When
+        val result = domainPersonToUIPersonMapperImpl.map(domainPerson).id
+
+        // Then
+        assertThat(result, `is`(expectedId))
+    }
+
+    @Test
+    fun mapShouldReturnUIPersonWithExpectedFirstNameGivenIdOnDomainPersonReturnsExpectedFirstName() {
+        // Given
+        val expectedFirstName = "${Random.nextInt()}"
+        every { domainPerson.firstName } returns expectedFirstName
+
+        // When
+        val result = domainPersonToUIPersonMapperImpl.map(domainPerson).firstName
+
+        // Then
+        assertThat(result, `is`(expectedFirstName))
+    }
+
+    @Test
+    fun mapShouldReturnUIPersonWithExpectedLastNameGivenIdOnDomainPersonReturnsExpectedLastName() {
+        // Given
+        val expectedLastName = "${Random.nextInt()}"
+        every { domainPerson.lastName } returns expectedLastName
+
+        // When
+        val result = domainPersonToUIPersonMapperImpl.map(domainPerson).lastName
+
+        // Then
+        assertThat(result, `is`(expectedLastName))
+    }
+
+    @Test
+    fun mapShouldReturnUIPersonWithExpectedBirthdayGivenIdOnDomainPersonReturnsExpectedBirthdayAndFormatOnBirthdayFormatReturnsExpectedFormattedBirthday() {
+        // Given
+        val expectedBirthday = Date(Random.nextLong())
+        every { domainPerson.birthday } returns expectedBirthday
+        val expectedFormattedBirthday = "${Random.nextLong()}"
+        every { birthdayDateFormat.format(expectedBirthday) } returns expectedFormattedBirthday
+
+        // When
+        val result = domainPersonToUIPersonMapperImpl.map(domainPerson).birthday
+
+        // Then
+        assertThat(result, `is`(expectedFormattedBirthday))
+    }
+}
