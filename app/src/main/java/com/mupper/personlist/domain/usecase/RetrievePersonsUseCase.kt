967f5e7ad0abb01e663d547ffceee6a006d43bca
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2021-04-22 23:10:58 -0700

    RetrievePersonsUseCase implemented

--- app/src/main/java/com/mupper/personlist/domain/usecase/RetrievePersonsUseCase.kt
+++ app/src/main/java/com/mupper/personlist/domain/usecase/RetrievePersonsUseCase.kt
@@ -1,13 +1,16 @@
 package com.mupper.personlist.domain.usecase
 
+import com.mupper.personlist.domain.repository.PersonRepository
 import javax.inject.Inject
 
 interface RetrievePersonsUseCase {
     suspend operator fun invoke()
 }
 
-class RetrievePersonsUseCaseImpl @Inject constructor() : RetrievePersonsUseCase {
+class RetrievePersonsUseCaseImpl @Inject constructor(
+    private val personRepository: PersonRepository
+) : RetrievePersonsUseCase {
     override suspend fun invoke() {
-        TODO("Not yet implemented")
+        personRepository.retrievePersons()
     }
 }
