3b0dce37b7e7c67b68dd364bdcc2177f670239a8
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2021-04-25 00:48:12 -0700

    UIPerson fullname and acronym

--- app/src/main/java/com/mupper/personlist/presentation/entity/UIPerson.kt
+++ app/src/main/java/com/mupper/personlist/presentation/entity/UIPerson.kt
@@ -1,11 +1,18 @@
 package com.mupper.personlist.presentation.entity
 
+import java.lang.IllegalArgumentException
+
 data class UIPerson(
     val id: String,
     val firstName: String,
     val lastName: String,
     val birthday: String,
 ) {
-    val fullName: String get() = "TODO()"
-    val acronym: String get() = "TODO()"
+    init {
+        if (firstName.isEmpty()) throw IllegalArgumentException()
+        if (lastName.isEmpty()) throw IllegalArgumentException()
+    }
+
+    val fullName: String get() = "$firstName $lastName"
+    val acronym: String get() = "${firstName.first()}${lastName.first()}"
 }
